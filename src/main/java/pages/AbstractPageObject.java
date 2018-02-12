07619c5498a70a78c1b2a72dd673d7e353543c17
nankoyel <nankoyel@gmail.com>
2018-02-11 22:57:00 -0700

    Update AbstractPageObject.java
    Removed unnecessary code

--- src/main/java/pages/AbstractPageObject.java
+++ src/main/java/pages/AbstractPageObject.java
@@ -7,7 +7,6 @@ public abstract class AbstractPageObject {
 
     protected WebDriver driver;
     final static int PAGE_TIME_OUT = 10;
-    // WebDriverWait wait;
 
     public AbstractPageObject(final WebDriver driver) {
         this(driver, PAGE_TIME_OUT);
@@ -15,7 +14,6 @@ public abstract class AbstractPageObject {
 
     public AbstractPageObject(final WebDriver driver, int timeout) {
         this.driver = driver;
-        // wait = new WebDriverWait(driver, timeout);
         PageFactory.initElements(driver, this);
     }
 
